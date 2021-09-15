3d07523ad76d911005b077d5ac3665bf9f4a6a27
Skrypi4 <Skrypi444@gmail.com>
2021-09-15 16:04:38 +0500

    not working yet

--- app/src/main/java/com/example/rickandmortynew/CharacterSpisok.kt
+++ app/src/main/java/com/example/rickandmortynew/CharacterSpisok.kt
@@ -3,6 +3,7 @@ package com.example.rickandmortynew
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ListView
+import android.widget.Toast
 import retrofit2.Call
 
 class CharacterSpisok : AppCompatActivity() {
@@ -12,21 +13,22 @@ class CharacterSpisok : AppCompatActivity() {
 
         var items: ArrayList<Response.CharactersSpisokItems>? = null
 
-        val service = ServiceBuilder.buildService(Service::class.java).charactersSpisok().enqueue(object: retrofit2.Callback<Response.CharactersSpisok>{
+        val service = ServiceBuilder.buildService(Service::class.java).charactersSpisok().enqueue(object : retrofit2.Callback<Response.CharactersSpisok> {
             override fun onResponse(
-                p0:  Call<Response.CharactersSpisok>?,
-                p1: retrofit2.Response<Response.CharactersSpisok>?
+                    p0: Call<Response.CharactersSpisok>?,
+                    p1: retrofit2.Response<Response.CharactersSpisok>?
             ) {
-                if (p1?.code() == 200){
+                if (p1?.code() == 200) {
                     items = p1?.body()?.data
-                    val adapter = AdapterCharacters(this@CharacterSpisok, R.layout.characters_strochka, items!!)
+                    val adapter = AdapterDlyaSpiska(this@CharacterSpisok, R.layout.characters_strochka, items!!)
                     findViewById<ListView>(R.id.listView).adapter = adapter
                 }
             }
 
-            override fun onFailure(call: Call<Response.CharactersSpisok>, t: Throwable) {
+            override fun onFailure(p0: Call<Response.CharactersSpisok>, p1: Throwable) {
                 TODO("Not yet implemented")
             }
-        })
+        }
+        )
     }
 }
